b54c88f3e018a997a1506235886e0654f77b46e2
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-09 11:37:52 -0400

    Prueba cypress completa: Si se ingresa una cadena con los delimitadores (- o ,) se muestra la suma de los numeros

--- cypress/e2e/sumarCadena.cy.js
+++ cypress/e2e/sumarCadena.cy.js
@@ -11,5 +11,11 @@ describe("Sumador", () => {
       cy.get("#calculadora-button").click();
       cy.get("#resultado-div").should("contain", 0);
     });
+    it("Cuando ingrese una cadena delimitadores establecidos (, o -), se debería mostrar la suma de los números.", () => {
+      cy.visit("/");
+      cy.get("#cadena").type("100,30-20");
+      cy.get("#calculadora-button").click();
+      cy.get("#resultado-div").should("contain", 150);
+    });
   });
   
