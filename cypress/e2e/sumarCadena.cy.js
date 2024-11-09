493065746c49687f1637a7296ff5f93f32dfee4e
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-09 10:39:28 -0400

    Prueba cypress completa: Si se ingresa un numero mayor a 1000 en la cadena, se ignora este numero

--- cypress/e2e/sumarCadena.cy.js
+++ cypress/e2e/sumarCadena.cy.js
@@ -1,7 +1,13 @@
 describe("Sumador", () => {
-    it("Muestra 0 si el usuario ingresa una cadena vacia", () => {
+    it("Cuando no ingrese ningún número y presione el botón calcular, se debería mostrar 0 como resultado", () => {
       cy.visit("/");
       cy.get("#cadena").clear();
+      cy.get("#calculadora-button").click();
+      cy.get("#resultado-div").should("contain", 0);
+    });
+    it("Cuando ingrese números mayores a 1000, estos deben ser ignorados en el cálculo", () => {
+      cy.visit("/");
+      cy.get("#cadena").type("10000");
       cy.get("#calculadora-button").click();
       cy.get("#resultado-div").should("contain", 0);
     });
